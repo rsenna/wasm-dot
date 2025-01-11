578cdcd7592cce8967df7c46d7225dd4ed5d3635
Rogério Senna [mb13] <rsennac@outlook.com>
2025-01-11 19:24:45 +0100

    Uppgrade www packages & fix settings
    -    copy-webpack-plugin: from ^5. 1. 1 to ^12. 0.2
    -    webpack:             from ^4.43. 0 to  ^5.97.1
    -    webpack-cli:         from ^3. 3.11 to  ^5. 1.4
    -    webpack-dev-server:  from ^3.11 .0 to  ^5. 2.0

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -3,12 +3,23 @@ const path = require('path');
 
 module.exports = {
   entry: "./bootstrap.js",
+  experiments: {
+    asyncWebAssembly: true,
+    syncWebAssembly: true
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
   },
   mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "index.html"),
+          to: path.resolve(__dirname, "dist", "index.html")
+        },
+      ],
+    }),
   ],
 };
